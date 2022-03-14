073ce8f294016f767f2e2f21632aad730f99aded
NBinte <nuzhatbintei@gmail.com>
2022-03-14 11:35:37 +0600

    Require without assigning

---
+++ fourthModule.js
@@ -0,0 +1,9 @@
+const num1 = 10;
+const num2 = 20;
+
+function addValues(){
+    console.log(`the sum is ${num1 + num2}`);
+}
+
+addValues();
+
--- modules.js
+++ modules.js
@@ -2,12 +2,13 @@ const names = require("./firstModule");
 const sayHi = require("./secondModule");
 const data = require("./thirdModule");
 
+require("./fourthModule");
 
 // console.log(names);
 // console.log(sayHi);
 // console.log(data);
 
 
-sayHi("boop");
-sayHi(names.tsuki);
-sayHi(names.shiba);
+// sayHi("boop");
+// sayHi(names.tsuki);
+// sayHi(names.shiba);
