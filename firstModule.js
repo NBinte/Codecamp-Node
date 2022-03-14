1f4b8dc6d07ca12d3acc8720796c6f7958e0d928
NBinte <nuzhatbintei@gmail.com>
2022-03-14 10:52:55 +0600

    Add modules

---
+++ firstModule.js
@@ -0,0 +1,8 @@
+//local
+const secret = "super secret";
+
+//share
+const tsuki = "tsuki";
+const shiba = "shiba";
+
+module.exports = { tsuki, shiba };
---
+++ modules.js
@@ -0,0 +1,10 @@
+const names = require("./firstModule");
+const sayHi = require("./secondModule");
+
+// console.log(names);
+// console.log(sayHi);
+
+
+sayHi("boop");
+sayHi(names.tsuki);
+sayHi(names.shiba);
